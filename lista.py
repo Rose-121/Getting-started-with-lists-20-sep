9e0759a25514083aeb7beb838b80215d7902288d
Rose <mehrabkhanrose10@gmail.com>
2024-09-20 19:05:38 +0600

    first commit

---
+++ lista.py
@@ -0,0 +1,15 @@
+names = ["Nusaiba", "Nabila", "Rose"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+
+names.append("Nahian Sir")
+
+print(names[3])
+print(names)
+
+print(len(names))
+
+alist = [100,200,300,400,500]
+print(alist[:: -1])
